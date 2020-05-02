6da63df2cfe4aa9eabc5b2c1971f678a94dee462
Jin Heo <993jin@gmail.com>
2020-05-02 12:27:02 -0400

    update working bionic core

--- ubuntu.sh
+++ ubuntu.sh
@@ -25,7 +25,7 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-		wget "https://partner-images.canonical.com/core/${core}/current/ubuntu-{$core}-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
+		wget "https://partner-images.canonical.com/core/${core}/current/ubuntu-bionic-core-cloudimg-${archurl}-root.tar.gz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
